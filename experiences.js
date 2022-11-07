d207e054d57668cf28bf7691fe721abd4ea2c547
Antoine Kahlouche <antoine@kahlouche.fr>
2022-11-07 15:14:33 -0500

    Modif trigger CI

--- experiences.js
+++ experiences.js
@@ -1,130 +1,130 @@
 const experiences = [
-	{
-		title: "Développeur JavaScript",
-		project: "Portfolio",
-		link: "https://antoine.kahlouche.fr/",
-		infos: {
-			company: "Antoine Kahlouche SASU",
-			location: "Courbevoie",
-			dates: "2 jours (févr. 2021)"
-		},
-		details: ["Création d'un portfolio de l'ensemble des mes expériences."],
-		tags: ["Git", "HTML/CSS", "JavaScript", "UX/UI"]
-	},
-	{
-		title: "Développeur Javascript",
-		project: "R4CE",
-		link: "https://r4ce.co",
-		infos: {
-			company: "Antoine Kahlouche SASU",
-			location: "Courbevoie",
-			dates: "2 ans 4 mois (sept. 2018 – déc. 2020)"
-		},
-		details: ["R4CE est une application qui aide les athlètes à trouver leur prochaine course.", "- Développement frontend et backend (VueJs, ExpressJs, MongoDB, Redis, Nginx, Node, AWS, Docker, GitHub Actions)", "- Manipulation de tuiles vectorielles (cartographie)"],
-		tags: ["AWS", "Communication", "Docker", "Git", "GitHubActions", "HTML/CSS", "JavaScript", "MongoDB", "Nginx", "NodeJS", "Redis", "UX/UI", "VueJS"]
-	},
-	{
-		title: "Développeur Javascript",
-		project: "SuperCuistot",
-		infos: {
-			company: "Antoine Kahlouche SASU",
-			location: "Courbevoie",
-			dates: "11 mois (mars 2017 – janv. 2018)"
-		},
-		details: ["Création d'une application de cuisine qui aide les gens à mieux manger chaque jour", "- Création d'une application mobile de bout en bout (react-native, firebase, code-push)", "- Utilisation de Play Store et de l'App Store", "L'application n'est plus maintenue"],
-		tags: ["Communication", "Firebase", "Git", "HTML/CSS", "JavaScript", "ReactJS", "UX/UI"]
-	},
-	{
-		title: "Développeur Java",
-		project: "SGE",
-		infos: {
-			contract: "CDI",
-			company: "Capgemini",
-			location: "Nanterre",
-			dates: "5 mois (oct. 2016 – févr. 2017)"
-		},
-		details: ["Membre de l'équipe d'un projet agile SGE (Système de Gestion des Echanges)", "- Développement (J2EE, OSB)", "Grand projet (~ 100 personnes)"],
-		tags: ["Java", "Scrum"]
-	},
-	{
-		title: "Développeur Java",
-		project: "GIPGE",
-		link: "https://www.guichet-entreprises.fr/fr/",
-		infos: {
-			contract: "Alternance",
-			company: "Capgemini",
-			location: "Paris",
-			dates: "3 mois (août 2015 – oct. 2015)"
-		},
-		details: ["Responsable de l'amélioration de la qualité du code du projet GIPGE (Guichet Entreprises)", "- Outils de configuration (Checkstyle, PMD)", "Projet moyen (~ 10 personnes)"],
-		tags: ["Java"]
-	},
-	{
-		title: "Développeur Java",
-		project: "ARGOS",
-		link: "",
-		infos: {
-			contract: "CDI",
-			company: "Capgemini",
-			location: "Suresnes",
-			dates: "2 ans 10 mois (janv. 2014 – oct. 2016)"
-		},
-		details: ["Gestion de l'activité de la Cour des comptes", "- Lead dev", "- Atelier clients", "- UX/UI (Axure)", "- Conception (UML)", "- Développement (J2EE, Spring, Hibernate, SVN, HTML, CSS)", "Projet moyen (~ 20 personnes)"],
-		tags: ["HTML/CSS", "Java", "JavaScript", "UML", "UX/UI", "Communication"]
-	},
-	{
-		title: "TOEIC",
-		infos: {
-			company: "Educational Testing Service (ETS)",
-			location: "Paris",
-			dates: "juil. 2013"
-		},
-		details: ["Obtention du TOEIC avec une note de 925/990", "Listening : 495/495", "Reading 430/495"],
-		tags: ["Anglais", "Diplôme"]
-	},
-	{
-		title: "Développeur Java",
-		project: "SIRHEN",
-		infos: {
-			contract: "Alternance",
-			company: "Capgemini",
-			location: "Suresnes",
-			dates: "2 ans 4 mois (sept. 2011 – déc. 2013)"
-		},
-		details: ["Création de SIRHEN (Système d'Information des Ressources Humaines de l'Education Nationale)", "- Développement (J2EE, Spring, Hibernate, SVN, HTML, CSS, Drools)", "Grand projet (~ 100 personnes)"],
-		tags: ["HTML/CSS", "Java", "JavaScript"]
-	},
-	{
-		title: "Ingénieur Informatique, Système d'information",
-		infos: {
-			company: "Conservatoire National des Arts et Métiers (CNAM)",
-			location: "Paris",
-			dates: "3 ans (sept. 2011 – juil. 2014)"
-		},
-		details: ["Obtention du diplôme d'ingénieur informatique et système d'information au CNAM, en alternance avec Capgemini."],
-		tags: ["Diplôme"]
-	},
-	{
-		title: "Développeur Java",
-		project: "CMS",
-		infos: {
-			contract: "Stage",
-			company: "NXP Semiconductors",
-			location: "Caen",
-			dates: "3 mois (avr. 2011 – juin 2011)"
-		},
-		details: ["Responsable de la création d'un CMS pour la mise à jour des instructions d'aide de l'intranet", "- Analyse de l'existant", "- Identification d'un modèle pour plus d'homogénéité dans les pages d'aide", "- Création d'un CMS à partir du modèle (Java)", "Petit projet (~1 personne)"],
-		tags: ["Java"]
-	},
-	{
-		title: "DUT Informatique",
-		link: "",
-		infos: {
-			company: "Université de Caen Basse Normandie (IUT)",
-			location: "Ifs",
-			dates: "2 ans (sept. 2009 – juil. 2011)"
-		},
-		details: ["Obtention du Diplôme Universitaire de Technologie (DUT) spécialité informatique.", "Equivalent : 120 crédits"],
-		tags: ["Diplôme"]
-	}
+    {
+        title: 'Développeur JavaScript',
+        project: 'Portfolio',
+        link: 'https://antoine.kahlouche.fr/',
+        infos: {
+            company: 'Antoine Kahlouche SASU',
+            location: 'Courbevoie',
+            dates: '2 jours (févr. 2021)'
+        },
+        details: ["Création d'un portfolio de l'ensemble des mes expériences."],
+        tags: ['Git', 'HTML/CSS', 'JavaScript', 'UX/UI']
+    },
+    {
+        title: 'Développeur Javascript',
+        project: 'R4CE',
+        link: 'https://r4ce.co',
+        infos: {
+            company: 'Antoine Kahlouche SASU',
+            location: 'Courbevoie',
+            dates: '2 ans 4 mois (sept. 2018 – déc. 2020)'
+        },
+        details: ['R4CE est une application qui aide les athlètes à trouver leur prochaine course.', '- Développement frontend et backend (VueJs, ExpressJs, MongoDB, Redis, Nginx, Node, AWS, Docker, GitHub Actions)', '- Manipulation de tuiles vectorielles (cartographie)'],
+        tags: ['AWS', 'Communication', 'Docker', 'Git', 'GitHubActions', 'HTML/CSS', 'JavaScript', 'MongoDB', 'Nginx', 'NodeJS', 'Redis', 'UX/UI', 'VueJS']
+    },
+    {
+        title: 'Développeur Javascript',
+        project: 'SuperCuistot',
+        infos: {
+            company: 'Antoine Kahlouche SASU',
+            location: 'Courbevoie',
+            dates: '11 mois (mars 2017 – janv. 2018)'
+        },
+        details: ["Création d'une application de cuisine qui aide les gens à mieux manger chaque jour", "- Création d'une application mobile de bout en bout (react-native, firebase, code-push)", "- Utilisation de Play Store et de l'App Store", "L'application n'est plus maintenue"],
+        tags: ['Communication', 'Firebase', 'Git', 'HTML/CSS', 'JavaScript', 'ReactJS', 'UX/UI']
+    },
+    {
+        title: 'Développeur Java',
+        project: 'SGE',
+        infos: {
+            contract: 'CDI',
+            company: 'Capgemini',
+            location: 'Nanterre',
+            dates: '5 mois (oct. 2016 – févr. 2017)'
+        },
+        details: ["Membre de l'équipe d'un projet agile SGE (Système de Gestion des Echanges)", '- Développement (J2EE, OSB)', 'Grand projet (~ 100 personnes)'],
+        tags: ['Java', 'Scrum']
+    },
+    {
+        title: 'Développeur Java',
+        project: 'GIPGE',
+        link: 'https://www.guichet-entreprises.fr/fr/',
+        infos: {
+            contract: 'Alternance',
+            company: 'Capgemini',
+            location: 'Paris',
+            dates: '3 mois (août 2015 – oct. 2015)'
+        },
+        details: ["Responsable de l'amélioration de la qualité du code du projet GIPGE (Guichet Entreprises)", '- Outils de configuration (Checkstyle, PMD)', 'Projet moyen (~ 10 personnes)'],
+        tags: ['Java']
+    },
+    {
+        title: 'Développeur Java',
+        project: 'ARGOS',
+        link: '',
+        infos: {
+            contract: 'CDI',
+            company: 'Capgemini',
+            location: 'Suresnes',
+            dates: '2 ans 10 mois (janv. 2014 – oct. 2016)'
+        },
+        details: ["Gestion de l'activité de la Cour des comptes", '- Lead dev', '- Atelier clients', '- UX/UI (Axure)', '- Conception (UML)', '- Développement (J2EE, Spring, Hibernate, SVN, HTML, CSS)', 'Projet moyen (~ 20 personnes)'],
+        tags: ['HTML/CSS', 'Java', 'JavaScript', 'UML', 'UX/UI', 'Communication']
+    },
+    {
+        title: 'TOEIC',
+        infos: {
+            company: 'Educational Testing Service (ETS)',
+            location: 'Paris',
+            dates: 'juil. 2013'
+        },
+        details: ['Obtention du TOEIC avec une note de 925/990', 'Listening : 495/495', 'Reading 430/495'],
+        tags: ['Anglais', 'Diplôme']
+    },
+    {
+        title: 'Développeur Java',
+        project: 'SIRHEN',
+        infos: {
+            contract: 'Alternance',
+            company: 'Capgemini',
+            location: 'Suresnes',
+            dates: '2 ans 4 mois (sept. 2011 – déc. 2013)'
+        },
+        details: ["Création de SIRHEN (Système d'Information des Ressources Humaines de l'Education Nationale)", '- Développement (J2EE, Spring, Hibernate, SVN, HTML, CSS, Drools)', 'Grand projet (~ 100 personnes)'],
+        tags: ['HTML/CSS', 'Java', 'JavaScript']
+    },
+    {
+        title: "Ingénieur Informatique, Système d'information",
+        infos: {
+            company: 'Conservatoire National des Arts et Métiers (CNAM)',
+            location: 'Paris',
+            dates: '3 ans (sept. 2011 – juil. 2014)'
+        },
+        details: ["Obtention du diplôme d'ingénieur informatique et système d'information au CNAM, en alternance avec Capgemini."],
+        tags: ['Diplôme']
+    },
+    {
+        title: 'Développeur Java',
+        project: 'CMS',
+        infos: {
+            contract: 'Stage',
+            company: 'NXP Semiconductors',
+            location: 'Caen',
+            dates: '3 mois (avr. 2011 – juin 2011)'
+        },
+        details: ["Responsable de la création d'un CMS pour la mise à jour des instructions d'aide de l'intranet", "- Analyse de l'existant", "- Identification d'un modèle pour plus d'homogénéité dans les pages d'aide", "- Création d'un CMS à partir du modèle (Java)", 'Petit projet (~1 personne)'],
+        tags: ['Java']
+    },
+    {
+        title: 'DUT Informatique',
+        link: '',
+        infos: {
+            company: 'Université de Caen Basse Normandie (IUT)',
+            location: 'Ifs',
+            dates: '2 ans (sept. 2009 – juil. 2011)'
+        },
+        details: ['Obtention du Diplôme Universitaire de Technologie (DUT) spécialité informatique.', 'Equivalent : 120 crédits'],
+        tags: ['Diplôme']
+    }
 ]
