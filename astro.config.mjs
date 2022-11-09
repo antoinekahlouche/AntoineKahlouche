4b35e099e60fe330e7758ec2caf2855e5731d5dd
Antoine Kahlouche <antoine@kahlouche.fr>
2022-11-09 15:01:37 -0500

    Optimisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
 
+// https://astro.build/config
 export default defineConfig({
     server: {
         host: true,
         port: 3000
     },
-    integrations: [svelte(), tailwind()],
+    integrations: [svelte(), tailwind(), compress(), sitemap(), robotsTxt()],
     site: 'https://antoine.kahlouche.fr'
 })
